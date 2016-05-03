deeabf067f8329b60dceba05548792e0c21b8002
David Lee <dlee@cetaris.com>
2016-05-03 13:12:17 -0400

    changed credentials

--- backend/user-routes.js
+++ backend/user-routes.js
@@ -8,8 +8,8 @@ var app = module.exports = express.Router();
 // XXX: This should be a database of users :).
 var users = [{
   id: 1,
-  username: 'gonto',
-  password: 'gonto'
+  username: 'foo',
+  password: 'foo'
 }];
 
 function createToken(user) {
